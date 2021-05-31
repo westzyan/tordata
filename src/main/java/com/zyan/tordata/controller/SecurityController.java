4de106d61cd635eec0c44cda0c7d41a82b3f69a7
KnightCicada <872323740@qq.com>
2021-05-31 22:52:33 +0800

    list

--- src/main/java/com/zyan/tordata/controller/SecurityController.java
+++ src/main/java/com/zyan/tordata/controller/SecurityController.java
@@ -3,6 +3,7 @@ package com.zyan.tordata.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.zyan.tordata.domain.Category;
+import com.zyan.tordata.domain.Data;
 import com.zyan.tordata.domain.Link;
 import com.zyan.tordata.domain.Node;
 import com.zyan.tordata.result.CodeMsg;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -31,18 +33,56 @@ public class SecurityController {
 
     @RequestMapping("/association_graph")
     @ResponseBody
-    public Result<String> getData(String fileName) throws FileNotFoundException {
-        StringBuilder sb = new StringBuilder();
-        try {
-            String str;
-            BufferedReader in = new BufferedReader(new FileReader(fileName));
-            while ((str = in.readLine()) != null) {
-                sb.append(str.trim());
-            }
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        String res = sb.toString();
+    public Result<String> getData() {
+        Node node = new Node();
+        Node node2 = new Node();
+        Link link = new Link();
+        Link link2 = new Link();
+        Category category = new Category();
+        Category category2 = new Category();
+
+        List<Object> list=new LinkedList<>();
+        List<Node> nodeList = new LinkedList<>();
+        List<Link> linkList = new LinkedList<>();
+        List<Category> categoryList = new LinkedList<>();
+
+        node.setId(1);
+        node.setName("key");
+        node.setSymbolSize(19.12381);
+        node.setX(-1.2);
+        node.setY(2.4);
+        node.setValue(11);
+        node.setCategory(1);
+
+        node2.setId(2);
+        node2.setName("value");
+        node2.setSymbolSize(19.12);
+        node2.setX(-1.2235);
+        node2.setY(2.465436);
+        node2.setValue(15451);
+        node2.setCategory(7);
+
+        link.setSource(1);
+        link.setTarget(2);
+
+        link2.setSource(4);
+        link2.setTarget(2);
+
+        category.setName("类目1");
+        category2.setName("类目4");
+
+        nodeList.add(node);
+        nodeList.add(node2);
+        linkList.add(link);
+        linkList.add(link2);
+        categoryList.add(category);
+        categoryList.add(category2);
+
+        list.add(nodeList);
+        list.add(linkList);
+        list.add(categoryList);
+
+        String res = JSON.toJSONString(list);
         if (res.length() == 0) {
             return Result.error(CodeMsg.NULL_DATA);
         }
@@ -52,6 +92,6 @@ public class SecurityController {
 
     public static void main(String[] args) throws FileNotFoundException {
         SecurityController securityController = new SecurityController();
-        securityController.getData("/home/ysr/下载/tordata/src/main/resources/static/json/data1.json");
+        securityController.getData();
     }
 }
--- src/main/java/com/zyan/tordata/domain/Data.java
+++ src/main/java/com/zyan/tordata/domain/Data.java
@@ -1,5 +1,31 @@
 package com.zyan.tordata.domain;
 
 public class Data {
+    private Node node;
+    private Link link;
+    private Category category;
 
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+
+    public Link getLink() {
+        return link;
+    }
+
+    public void setLink(Link link) {
+        this.link = link;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
