d351edea2628efb387ac275cd56665a65d8ff1b2
westzyan <15639067131@163.com>
2021-06-04 17:03:39 +0800

    安全评估提供借口

--- src/main/java/com/zyan/tordata/TordataApplication.java
+++ src/main/java/com/zyan/tordata/TordataApplication.java
@@ -1,11 +1,13 @@
 package com.zyan.tordata;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class TordataApplication {
 
     public static void main(String[] args) {
